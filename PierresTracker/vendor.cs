e18b10e65b9d0844c195c59ef91936e7b80cc9a5
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-09-24 13:53:39 -0700

    add code for Vendor functionality

---
+++ PierresTracker/vendor.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+
+namespace PierresTracker.Models
+{
+  public class Vendor
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public List<Order> Orders { get; set; } 
+
+    public Vendor(string vendorName, string description)
+    {
+      Name = vendorName;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
+    public void RemoveOrder(Order order)
+    {
+      Orders.Remove(order);
+    }
+
+    public void RemoveVendor(Vendor vendor)
+    {
+      _instances.Remove(vendor);
+    }
+
+  }
+}
